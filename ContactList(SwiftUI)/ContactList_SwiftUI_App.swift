91e63b4aab73d2e0721d95c0f6e186c49aa3a7ca
BorisSonin <bsonin85@gmail.com>
2023-01-24 18:50:20 +0300

    Initial Commit

---
+++ ContactList(SwiftUI)/ContactList_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUI_App.swift
+//  ContactList(SwiftUI)
+//
+//  Created by Борис Сонин on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
